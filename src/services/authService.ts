2fcfdef60ba51243a925146574ba4551ddb27127
MarcoENri <130495181+MarcoENri@users.noreply.github.com>
2026-02-11 15:40:25 -0500

    Refactor authService to use dynamic baseURL

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_URL;
+
 export const api = axios.create({
-  baseURL: "https://investigation-deutschland-accessory-loud.trycloudflare.com",
+  baseURL,
 });
 
 api.interceptors.request.use(
@@ -21,13 +23,11 @@ api.interceptors.response.use(
   (err) => {
     const status = err?.response?.status;
 
-    // ⚠️ RECOMENDADO: limpia sesión solo con 401 (token inválido/expirado)
     if (status === 401) {
       localStorage.clear();
       window.location.href = "/";
     }
 
-    // Si es 403 puede ser "no tienes permiso" (NO necesariamente token malo)
     return Promise.reject(err);
   }
 );
